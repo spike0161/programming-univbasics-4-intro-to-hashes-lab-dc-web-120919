39a96712666848c17117fd41e4407a61095049d9
Dean Hildebrand <dean.hildebrand88@gmail.com>
2019-08-28 18:38:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,9 +26,17 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
+  
   # given a hash an a key as parameters, return an updated hash
+  
   updated_hash = Hash.new
   return updated_hash
+  
+  
   # if the provided key is not present in the hash, add it and assign it to the value of 1
+  
+  
+  
+  
   # if the provided key is present, increment its value by 1
 end
