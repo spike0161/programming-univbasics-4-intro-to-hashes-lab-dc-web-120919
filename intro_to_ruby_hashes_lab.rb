b214e177afbd42ab3286b4b56d32dddaa6ac457f
Dean Hildebrand <dean.hildebrand88@gmail.com>
2019-08-28 19:15:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,11 +25,13 @@ def read_from_hash(hash, key)
   
 end
 
+
+updated_hash = Hash.new
 def update_counting_hash(hash, key)
   
   # given a hash an a key as parameters, return an updated hash
 
-updated_hash = Hash.new
+
 
   
    # if the provided key is not present in the hash, add it and assign it to the value of 1
